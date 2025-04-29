d3ab488ca77e482f36ac4921cdcf6fd6906862eb
TheZink <ilkka.sinkonen@gmail.com>
2025-04-29 09:20:34 +0300

    Fix server startup log to correctly format IP and PORT

--- app.js
+++ app.js
@@ -42,9 +42,9 @@ mongoose.connect(process.env.MONGO_URI)
 
         // Start server only after DB connects
         const PORT = process.env.PORT || 3000;
-        const IP = process.env.IP
+        const IP = process.env.IP || 'localhost'
         app.listen(PORT, () => {
-            console.log(`🚀 Server running at http://${IP}${PORT}`);
+            console.log(`🚀 Server running at http://${IP}':'${PORT}`);
         });
     })
     .catch((err) => {
