992c5c258cb4223bfc7e6e8e2110f6bf7cc90199
TheZink <ilkka.sinkonen@gmail.com>
2025-05-03 12:17:53 +0300

    Lisätty endpoint oman tilin poistamiseen

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {body} = require('express-validator');
-const {newUser, allUsers, deleteUser, putUser} = require('../controllers/UserController');
+const {newUser, allUsers, deleteUser, deleteAccount, putUser} = require('../controllers/UserController');
 const {authenticateToken} = require('../middlewares');
 
 router.route('/user').post(
@@ -18,5 +18,6 @@ router.route('/:id').delete(authenticateToken, deleteUser).put(
     body('salasana').trim().isLength({min: 8}).escape(),
     putUser
 )
+router.route('/purge/:id').delete(authenticateToken, deleteAccount)
 
 module.exports = router;
